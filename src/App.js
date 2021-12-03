0655a4b5ac73d192afab64c95bda0dd6b387d916
aiacuone <aiacuone@users.noreply.github.com>
2021-12-03 11:16:55 +0000

    click counter project with tests basics complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [error, setError] = useState(false)
   return (
-    <div className="App">
-      <h1>Learn React Testing</h1>
+    <div className="App" data-test="component-app">
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;<span data-test="count">{count}</span>
+      </h1>
+      <button
+        data-test="decrement-button"
+        onClick={() => {
+          count > 0 ? setCount(count - 1) : setError(true)
+        }}>
+        Decrement
+      </button>
+      <button
+        data-test="increment-button"
+        onClick={() => {
+          if (error && count == 0) setError(false)
+          setCount(count + 1)
+        }}>
+        Increment
+      </button>
+      {error && <h1 style={{ color: 'red' }}>Error</h1>}
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,55 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+const setup = () => shallow(<App />)
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
+test('renders without error', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'component-app')
+  expect(appComponent.length).toBe(1)
+})
+
+test('renders increment button', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'increment-button')
+  expect(appComponent.length).toBe(1)
+})
+
+test('renders counter display', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'counter-display')
+  expect(appComponent.length).toBe(1)
+})
+
+test('counter starts at 0', () => {
+  const wrapper = setup()
+  const count = findByTestAttr(wrapper, 'count').text()
+  expect(count).toBe('0')
+})
+
+test('clicking button incremements counter display', () => {
+  const wrapper = setup()
+
+  //find the button
+  const button = findByTestAttr(wrapper, 'increment-button')
+
+  //click the button
+  button.simulate('click')
+
+  //Find the display, and test that the number has been incremented
+  const count = findByTestAttr(wrapper, 'count').text()
+
+  expect(count).toBe('1')
+})
+
+test('clicking decrement button decrements counter display', () => {
+  const wrapper = setup()
+  const button = findByTestAttr(wrapper, 'decrement-button')
+  button.simulate('click')
+  const count = findByTestAttr(wrapper, 'count').text()
+  expect(count).toBe('0')
+})
